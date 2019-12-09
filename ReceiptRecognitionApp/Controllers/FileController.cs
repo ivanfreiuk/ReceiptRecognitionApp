5f67e86fede0374e54717908b33c43f31163d5d1
Andriy Galas <halasandreyko@gmail.com>
2019-12-09 21:32:58 +0200

    Add FileController

---
+++ ReceiptRecognitionApp/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReceiptRecognitionApp.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            using (var stream = new MemoryStream())
+            {
+                await file.OpenReadStream().CopyToAsync(stream);
+                var data = stream.ToArray();
+            }
+
+            return Ok();
+        }
+    }
+}
