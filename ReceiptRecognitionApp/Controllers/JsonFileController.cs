0efd4ad735d2491d3c4506f62b16bf540875191e
Andriy Galas <halasandreyko@gmail.com>
2019-12-12 00:59:20 +0200

    Connect to Second Api and change response model add Constatnts class changed View for JsonFile

--- ReceiptRecognitionApp/Controllers/JsonFileController.cs
+++ ReceiptRecognitionApp/Controllers/JsonFileController.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using ReceiptRecognitionApp.Entities;
@@ -11,7 +15,7 @@ namespace ReceiptRecognitionApp.Controllers
     {
         private readonly IReceiptImageService _receiptImageService;
         private readonly IReceiptService _receiptService;
-
+        private const string ApiUrl = "http://localhost:54292/api/receipts";
         public JsonFileController(IReceiptImageService receiptImageService, IReceiptService receiptService)
         {
             _receiptImageService = receiptImageService;
@@ -41,35 +45,38 @@ namespace ReceiptRecognitionApp.Controllers
             });
         }
         
-        public IActionResult FormJson(int id)
+        public async Task<IActionResult> FormJson(int id)
         {
             //here should be called api to get response and convert it to ReceiptResponseModel
-            var response = new ReceiptResponseModel
-            {
-                ABN = "ABN",
-                Businessname = "Businessname",
-                Language = "Language",
-                Lines = new List<string>
-                {
-                    "Line 1",
-                    "Line 2",
-                    "Line 3"
-                },
-                ReceiptDate = "29/03/11 23:12:12",
-                ReceiptTotal = "12212,12",
-                Text = "Some text"
-            };
+            //var response = new ReceiptResponseModel
+            //{
+            //    ABN = "ABN",
+            //    Businessname = "Businessname",
+            //    Language = "Language",
+            //    Lines = new List<string>
+            //    {
+            //        "Line 1",
+            //        "Line 2",
+            //        "Line 3"
+            //    },
+            //    ReceiptDate = "29/03/11 23:12:12",
+            //    ReceiptTotal = "12212,12",
+            //    Text = "Some text"
+            //};
             var receiptImage = _receiptImageService.Get(id);
+            var url = await RequestReceiptProcessing(receiptImage.OriginalImage);
+            Task.Delay(5000).Wait();
+            var receipt = await GetReceiptResult(url);
             var data = new JsonFileViewModel
             {
                 ReceiptImage = receiptImage,
                 Receipt = new Receipt
                 {
-                    ReceiptDate = response.ReceiptDate,
-                    ReceiptTotal = response.ReceiptTotal,
+                    ReceiptDate = receipt.ReceiptDate,
+                    ReceiptTotal = receipt.ReceiptTotal,
                     ReceiptImageId = id,
-                    Text = response.Text,
-                    Json = JsonConvert.SerializeObject(response)
+                    Text = receipt.Text,
+                    Json = JsonConvert.SerializeObject(receipt)
                 }
             };
             SaveJson(data);
@@ -86,6 +93,26 @@ namespace ReceiptRecognitionApp.Controllers
                 Text = model.Receipt.Text,
                 Json = model.Receipt.Json
             });
+        }
+
+        private static async Task<string> RequestReceiptProcessing(byte[] receiptImage)
+        {
+            var client = new HttpClient();
+            var content = new ByteArrayContent(receiptImage);
+            content.Headers.ContentType = new MediaTypeHeaderValue(mediaType: "application/octet-stream");
+            var response = await client.PostAsync(ApiUrl, content);
+            var json = await response.Content.ReadAsStringAsync();
+            dynamic result = JsonConvert.DeserializeObject<object>(json);
+            return result.operationLocation;
+        }
+
+        private static async Task<ReceiptResponseModel> GetReceiptResult(string url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync($"{ApiUrl}?uri={url}");
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ReceiptResponseModel>(json);
+            return result;
         }
     }
 }
